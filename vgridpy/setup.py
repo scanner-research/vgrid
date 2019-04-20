5ca8092b9e1252317416be8e3ca6e03ae39f2ab0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-20 23:15:11 +0000

    Deploy scanner-research/vgrid to github.com/scanner-research/vgrid.git:gh-pages

--- vgridpy/setup.py
+++ vgridpy/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name='vgridpy',
-        version='0.1.4',
+        version='0.1.5',
         description='Python bindings into Vgrid objects',
         url='http://github.com/scanner-research/vgrid',
         author='Xinwei Yao, Will Crichton',
