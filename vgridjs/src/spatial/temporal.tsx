564b461cda6c41c0b2437bd35ee5c323db52933a
James Hong <james.hong@cs.stanford.edu>
2019-05-12 03:53:14 -0700

    Make SpatialType_Temporal a singleton

--- vgridjs/src/spatial/temporal.tsx
+++ vgridjs/src/spatial/temporal.tsx
@@ -2,10 +2,23 @@ import {SpatialType} from './spatial_type';
 
 export class SpatialType_Temporal extends SpatialType {
 
+  private static instance: SpatialType_Temporal;
+
+  private constructor() {
+    super();
+  }
+
   draw_view(): null { return null; }
   label_view(): null { return null; }
 
+  static get_instance() {
+    if (!SpatialType_Temporal.instance) {
+      SpatialType_Temporal.instance = new SpatialType_Temporal();
+    }
+    return SpatialType_Temporal.instance;
+  }
+
   static from_json(args: any): SpatialType_Temporal {
-    return new SpatialType_Temporal();
+    return SpatialType_Temporal.get_instance();
   }
 }
