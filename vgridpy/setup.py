a79f23e9278afd9e52e15fc999b7e5457baa97e1
Will Crichton <wcrichto@cs.stanford.edu>
2019-06-09 17:54:41 -0700

    Bump to 0.1.13

--- vgridpy/setup.py
+++ vgridpy/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 if __name__ == "__main__":
     setup(name='vgridpy',
-          version='0.1.12',
+          version='0.1.13',
           description='Python bindings into Vgrid objects',
           url='http://github.com/scanner-research/vgrid',
           author='Xinwei Yao, Will Crichton',
