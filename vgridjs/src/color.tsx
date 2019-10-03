ff5d2fcf1382b70d60d49354753f430235b04f24
Dan Fu <danfu09@gmail.com>
2019-10-03 10:47:00 -0700

    Change colors to tableau defaults

--- vgridjs/src/color.tsx
+++ vgridjs/src/color.tsx
@@ -1,3 +1,3 @@
-export let default_palette = [ "#db5784", "#b9db57", "#57db5f", "#db57b9", "#dbc957", "#57b9db", "#57db94", "#c957db", "#5f57db", "#db5f57", "#db9457", "#9457db", "#5784db", "#84db57", "#57dbc9"];
+export let default_palette = [ "#4e79a7", "#f28e2b", "#e15759", "#76b7b2", "#59a14f", "#edc948", "#b07aa1", "#ff9da7", "#9c755f", "#bab0ac"];
 
 export type ColorMap = {[key: string]: string};
