9da1b3766cde0715867253629ec33c19cd097855
Dan Fu <danfu09@gmail.com>
2019-10-14 18:16:20 -0700

    Push to v0.1.17

--- vgridpy/setup.py
+++ vgridpy/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 if __name__ == "__main__":
     setup(name='vgridpy',
-          version='0.1.16',
+          version='0.1.17',
           description='Python bindings into Vgrid objects',
           url='http://github.com/scanner-research/vgrid',
-          author='Xinwei Yao, Will Crichton',
-          author_email='yaodavid@stanford.edu',
+          author='Dan Fu, Xinwei Yao, Will Crichton',
+          author_email='danfu@cs.stanford.edu',
           license='Apache 2.0',
           packages=['vgrid'],
           install_requires=['rekallpy>=0.3.0'],
